bd2033cbd83593f8018522e27c53c5144b442739
Toni-Ty <Antonique2479@yahoo.com>
2021-12-15 19:50:32 -0600

    capitalize arena name

--- arena.js
+++ arena.js
@@ -1,7 +1,10 @@
 class Arena {
     constructor(name){
-        this.name = name.charAt(0).toUpperCase() 
+        this.name = name.charAt(0).toUpperCase() + name.substring(1, 20); 
         
 
     }
 }
+
+const colosseum = new Arena("colosseum");
+console.log(colosseum.name);
